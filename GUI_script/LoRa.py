5ce39043ade02a076b1c4e262dbcfe6073be410c
Park <parkchuy7077@gmail.com>
2025-03-17 09:29:09 +0900

    오타 수정

--- GUI_script/LoRa.py
+++ GUI_script/LoRa.py
@@ -2,7 +2,7 @@ from time import sleep
 from SX127x.LoRa import LoRa, MODE
 from SX127x.board_config import BOARD
 
-# Raspberry Pi 핀 구성 초기화
+# GPIO 핀 구성 초기화
 BOARD.setup()
 
 class LoRaReceiver(LoRa):
@@ -12,13 +12,13 @@ class LoRaReceiver(LoRa):
         self.set_dio_mapping([0] * 6)
 
     def start(self):
-        self.set_freq(915.0)  # Teensy 송신 코드와 동일한 주파수 설정
+        self.set_freq(915.0)  # 주파수 설정 (915 MHz)
         self.set_pa_config(pa_select=1)
         self.set_spreading_factor(7)  # Spreading Factor 설정
-        self.set_bw(7)  # Bandwidth (125kHz)
-        self.set_coding_rate(5)  # Coding Rate (4/5)
-        self.set_preamble(8)  # Preamble 길이
-        self.set_mode(MODE.RXCONT)  # Continuous RX 모드
+        self.set_bw(7)  # Bandwidth 설정 (125 kHz)
+        self.set_coding_rate(5)  # Coding Rate 설정 (4/5)
+        self.set_preamble(8)  # Preamble 길이 설정
+        self.set_mode(MODE.RXCONT)  # Continuous RX 모드 설정
         print("LoRa Receiver is started and waiting for incoming data...")
 
     def on_rx_done(self):
@@ -33,7 +33,7 @@ try:
     lora.start()
 
     while True:
-        sleep(1)  # 계속 데이터를 대기
+        sleep(1)  # 계속 대기하면서 데이터를 수신
 except KeyboardInterrupt:
     print("Program interrupted by user.")
 finally:
